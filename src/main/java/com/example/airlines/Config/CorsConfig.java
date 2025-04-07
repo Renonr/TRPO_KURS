38d2603f0cd0935434033852bd4849b5648462c7
Renonr <117162748+Renonr@users.noreply.github.com>
2025-04-07 13:51:02 +0300

    Update CorsConfig.java

--- src/main/java/com/example/airlines/Config/CorsConfig.java
+++ src/main/java/com/example/airlines/Config/CorsConfig.java
@@ -1,14 +1,13 @@
 package com.example.airlines.Config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
